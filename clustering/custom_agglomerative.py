8a7798910076e1f86cb1a17a38dd7d96463657e6
Anatoly.s <tolik.frnk@yandex.ru>
2022-10-06 20:57:29 +0300

    different metrics & methods  for clustering

--- clustering/custom_agglomerative.py
+++ clustering/custom_agglomerative.py
@@ -40,33 +40,42 @@ class AgglomerativeAlgorithm:
 
         # structure for clusters
         self.clusters = dict()
+        self.n_of_clusters = number_of_clusters
 
 
-    ######################################### distances
+    ######### distances between instances
     @staticmethod
     def euclidean_distance(vec1, vec2):
         return np.sqrt(np.sum((vec1-vec2)**2))
+    
     @staticmethod
     def manhattan_distance(vec1, vec2):
         return np.sum(np.abs(vec1-vec2))
+    
     @staticmethod
     def chebyshev_distance(vec1, vec2):
         return np.max(np.abs(vec1-vec2))
 
+    ######### distances between clusters
+    # 'new_c' is union of 'c1' and 'c2': new_c = c1 | c2
+    # 'c' is the cluster to which we calculate the distance
     
     def single_linkage(self, c1, c2, new_c, c):
+        """ R(new_c, c) = min(R(c1, c), R(c2, c)) """
         dist = min(self.clusters[c1]["distances"][c], self.clusters[c2]["distances"][c])
         self.clusters[new_c]["distances"][c] = dist
         self.clusters[c]["distances"][new_c] = dist
     
     def complete_linkage(self, c1, c2, new_c, c):
-        dist = min(self.clusters[c1]["distances"][c], self.clusters[c2]["distances"][c])
+        """ R(new_c, c) = max(R(c1, c), R(c2, c)) """
+        dist = max(self.clusters[c1]["distances"][c], self.clusters[c2]["distances"][c])
         self.clusters[new_c]["distances"][c] = dist
         self.clusters[c]["distances"][new_c] = dist
     
     
     def average_linkage():
         pass
+        
 
     
     def centroid_linkage():
@@ -79,62 +88,43 @@ class AgglomerativeAlgorithm:
     ################################# methods for algorithm
     def initial(self, data):
         """
-        create initial distance matrix using data, defined metric; 
-        also the structure for clusters is created here;
+        create initial distance matrix using data, defined metric (distance function for objects); 
+        also the structure for clusters is being created here;
         """
         for i in range(len(data)):
             # initial distance matrix: count distance for each pair (counted twice now)
             self.clusters[i] = {
                 "cluster_length": 1,      # the number of element in the cluster
                 "cluster": [i],           # the cluster itself
-                "distances": dict((j, self.metric(data[i], data[j])) for j in range(0, len(data)))
+                "distances": dict((j, self.metric(data[i], data[j])) for j in range(i+1, len(data)))
             }
-            del self.clusters[i]['distances'][i]
+        for i in range(len(data)):
+            for j in range(0, i):
+                self.clusters[i]['distances'][j] = self.clusters[j]['distances'][i]
 
 
     def find_min_distance(self):
         """
         function to find the minimum element in the distances matrix;
-        find 2 minimum (first for merging two clusters, second for optimization)
         """
+        # idea for optimization: find 2 minimum (first for merging two clusters, second for optimization)
         min_dist = 1e6
-        first_pair_of_clusters = ()
         for i in self.clusters:
-            for j in self.clusters[i]["distances"]:
-                if self.clusters[i]["distances"][j] < min_dist:
-                    min_dist = self.clusters[i]["distances"][j]
-                    pair_of_clusters = (i, j)
+            d = self.clusters[i]["distances"]
+            index_for_min_in_current_cluster = min(d, key=d.get)
+            if d[index_for_min_in_current_cluster] < min_dist:
+                min_dist = d[index_for_min_in_current_cluster]
+                pair_of_clusters = (i, index_for_min_in_current_cluster)
         return pair_of_clusters, min_dist
     
     
-    def linkage(self, u, v, new_index):
-        # w = u || v - new cluster
-        # distance is calculated by formula:
-        # R(w, s) = alpha 
-        if self.method == "single":            
-            for s in self.clusters:
-                if s not in [u, v, new_index]:
-                    self.clusters[s][new_index] = min(self.clusters[u][s], self.clusters[v][s])
-                    self.clusters[new_index][s] = self.clusters[s][new_index]
-
-        elif self.method == "complete":
-             for s in self.clusters:
-                if s not in [u, v, new_index]:
-                    self.clusters[s][new_index] = max(self.clusters[u][s], self.clusters[v][s])
-                    self.clusters[new_index][s] = self.clusters[s][new_index]
-
-        elif self.method == "average":
-            # future
-            pass
-        elif self.method == "centroid":
-            # future
-            pass
-        elif self.method == "word":
-            # future
-            pass
-
-        else:
-            return 
+    def linkage(self, c1, c2, new_c):
+        """
+        function to calculate the distances between new cluster and others
+        """
+        for c in self.clusters:
+            if c not in [new_c, c1, c2]:
+                self.method(c1, c2, new_c, c)
 
 
     def merge_two_clusters(self, c1, c2, new_c):
@@ -143,10 +133,10 @@ class AgglomerativeAlgorithm:
         """
         # firstly, create the new cluster
         self.clusters[new_c] = {
-            "cluster_length": self.clusters[c1]["length"] + self.clusters[c2]["length"], 
-            "cluster": self.cluster[c1]["cluster"] + self.cluster[c2]["cluster"],
-            "distances": 0
-            }
+            "cluster_length": self.clusters[c1]["cluster_length"] + self.clusters[c2]["cluster_length"], 
+            "cluster": self.clusters[c1]["cluster"] + self.clusters[c2]["cluster"],
+            "distances": dict()
+        }
 
         
         # secondly, count the distance to new cluster from other
@@ -166,19 +156,23 @@ class AgglomerativeAlgorithm:
         function corresponding to the main algorithm
         """
         n = len(X)
+        # create initial clusters
         self.initial(X)
+        
         # linkage matrix is need for visualizing
         linkage_matrix = np.zeros([n-1, 4])
 
         # iterations
         for t in range(0, n-1):
             pair, dist = self.find_min_distance()
-            u, v = pair
-            self.merge_two_clusters(u, v, n+t)
-            # fill the linkage matrix according to the requirements: 
-            linkage_matrix[t][0] = u
-            linkage_matrix[t][1] = v
-            linkage_matrix[t][2] = dist
-            linkage_matrix[t][3] = self.clusters[n+t][-1]
+            c1, c2 = pair
 
+            self.merge_two_clusters(c1, c2, n+t)
+            
+            # fill the linkage matrix according to the requirements for creating dendrogram: 
+            linkage_matrix[t][0] = c1
+            linkage_matrix[t][1] = c2
+            linkage_matrix[t][2] = dist
+            linkage_matrix[t][3] = self.clusters[n+t]['cluster_length']
+        
         return linkage_matrix
